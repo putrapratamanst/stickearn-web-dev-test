e44024a39e4cf9ce06d495b3a9f6ae373723a664
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-03 02:06:24 +0700

    testing responsive

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,15 +2,10 @@
 
 <body><div class="container">
     <div class="row">
-        <div class="col-md-2">
-        </div>
         <div class="col-md-8 middle-content">
             <!-- /.content -->
             @yield('content')
             <!-- /.content -->
-        </div>
-        <div class="col-md-2">
-
         </div>
     </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.main')
 @section('content')
-
+<!-- 
 <div class="preloader">
     <div class="loading">
         <img src="{{ asset('assets/images/logo.gif') }}">
     </div>
-</div>
+</div> -->
 
 <h3 style="text-align: center;">Stickearn Web Developer Test </h3>
 <h5 style="text-align: center;">(Word Scrambler)</h5>
-
 <p style="text-align: center;">
-    <img src="{{ asset('assets/images/logo.jpg') }}" width="350" height="350" ">
-    <span style=" font-family: 'Nunito Sans' ;font-size: 22px;"> Click <a href="/player/form" class="btn btn-dark blink">PLAY</a> To Start the Game</span>
-
+    <img class="logo" src="{{ asset('assets/images/logo.jpg') }}" width="350" height="350">
+    <span style=" font-family: 'Nunito Sans' ;font-size: 22px;"> Click <a href="/player/form" class="btn btn-dark blink">PLAY</a> To Start the Game</span>  
 </p>
 <div class="dashboard-admin">
     <a href="/home">
