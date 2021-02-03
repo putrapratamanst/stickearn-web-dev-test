f87de42da95b57f64cef5908172770e568a7e6c9
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-03 15:18:20 +0700

    create new player if not exist

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -2,8 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
+use Illuminate\Http\Request;
+use App\Repository\PlayerRepository;
+use App\Traits\PlayerTrait;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+
 class PlayerController extends Controller
 {
+    use PlayerTrait;
     /**
      * Show the application dashboard.
      *
@@ -12,5 +20,28 @@ class PlayerController extends Controller
     public function form()
     {
         return view('/player/form');
+    }
+
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), Player::$rules);
+
+        if ($validator->fails())
+            return Redirect::to('/player/form')->withInput()->withErrors($validator);
+
+        $dataPlayer = $this->detailByUsername($request->username);
+
+        if ($dataPlayer) {
+            $checkPlayer = $this->detailByUsernameAndPassword($request->username, $request->password);
+            if (!$checkPlayer) {
+                return view('/player/form')->withErrors(['', 'Data Player Not Found ']);
+            }
+        } else {
+            $dataPlayer = $this->createPlayer($request);
+        }
+
+        $this->setSessionPlayer($dataPlayer);
+
+        return redirect('/scrambler/playground');
     }
 }
