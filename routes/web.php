506be4b0892796dde79b90899dd28b060b8b825b
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-04 01:46:55 +0700

    fix register routing history, logout, home

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\ScramblerController;
 use App\Http\Controllers\ScoreController;
+use App\Http\Controllers\ResultController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +17,13 @@ use App\Http\Controllers\ScoreController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+$router->get('logout', [LoginController::class, 'logout']);
+
 $router->get('/', function () {
     return view('welcome');
 });
+$router->get('home', [HomeController::class, 'index']);
 
 $router->group(['prefix' => 'player'], function () use ($router) {
     $router->get('form', [PlayerController::class, 'form']);
@@ -29,4 +36,12 @@ $router->group(['prefix' => 'scrambler'], function () use ($router) {
     $router->post('check', [ScramblerController::class, 'check']);
 });
 
-$router->get('/score/get', [ScoreController::class, 'getScore']);
+$router->group(['prefix' => 'score'], function () use ($router) {
+    $router->get('get', [ScoreController::class, 'getScore']);
+    $router->get('delete', [ScoreController::class, 'delete']);
+});
+
+$router->group(['prefix' => 'result'], function () use ($router) {
+    $router->get('history', [ResultController::class, 'history']);
+    $router->get('admin/history/{playerId}', [ResultController::class, 'historyInAdmin']);
+});
