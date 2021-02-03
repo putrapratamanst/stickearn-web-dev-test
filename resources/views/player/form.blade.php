7cc7582dc45792f902e1e4d524f711cbcdebb472
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-03 12:07:32 +0700

    fix form register login new user

---
+++ resources/views/player/form.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.main')
+
+@section('content')
+
+
+{{ Form::open(array('url' => 'player/create', 'autocomplete'=>'off')) }}
+
+<center>
+    <h2>Login/Register Game Player</h2>
+</center>
+<hr>
+@if ($errors->any())
+<div class="alert alert-danger" style="text-align: center;">
+    <ul>
+        @foreach ($errors->all() as $error)
+        {{ $error }}
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<div id="form-player">
+    <center>
+        {{ Form::text('username', '', ['required',  'placeholder' => 'Please Input Username','class' => 'form-input']) }}
+        {{ Form::password('password', ['required',  'placeholder' => 'Please Input Password','class' => 'form-input']) }}
+
+        {{ Form::submit('Submit', ['class' => 'btn btn-stickearn', 'style' => 'margin:10px']) }}
+    </center>
+</div>
+
+
+{{ Form::close() }}
+
+@endsection
