c979ce24ca1bc15f1a3e1fbcae65c949bb7789e6
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-03 20:09:34 +0700

    save result

---
+++ app/Repository/ResultRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\Result;
+
+class ResultRepository
+{
+    protected $playerId;
+    protected $word;
+
+    public function setPlayerId($playerId)
+    {
+        $this->playerId = $playerId;
+    }
+
+    public function setWord($word)
+    {
+        $this->word = $word;
+    }
+
+    public function resultByPlayerId()
+    {
+        return Result::where('player_id', $this->playerId)
+            ->where('original_word', $this->word)
+            ->first();
+    }
+
+    public static function list($idPlayer)
+    {
+        return Result::where('player_id', $idPlayer)
+            ->orderByDesc('id')
+            ->get();
+    }
+
+    public static function listAll($idPlayer)
+    {
+        return Result::where('player_id', $idPlayer)
+            ->orderByDesc('id')
+            ->get();
+    }
+}
---
+++ app/Traits/ResultTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Result;
+use App\Repository\ResultRepository;
+
+trait ResultTrait
+{
+    public static function resultByPlayer($playerId)
+    {
+        $repo = new ResultRepository();
+        $repo->setPlayerId($playerId);
+        return $repo->resultByPlayerId();
+    }
+
+    public static function saveResult($playerId, $guessWord, $originalWord, $scrambleWord, $status)
+    {
+        $model                 = new Result();
+        $model->player_id      = $playerId;
+        $model->answer         = $guessWord;
+        $model->scramble_word  = $scrambleWord;
+        $model->original_word  = $originalWord;
+        $model->status         = $status;
+
+        $model->save();
+    }
+}
