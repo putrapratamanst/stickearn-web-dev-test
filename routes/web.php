08cfc6add09aeccb8dded7672ec7cc19688e20fa
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-03 16:19:06 +0700

    register routing for show playground scrambler

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\ScramblerController;
+use App\Http\Controllers\ScoreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,10 @@ $router->group(['prefix' => 'player'], function () use ($router) {
     $router->get('form', [PlayerController::class, 'form']);
     $router->post('create', [PlayerController::class, 'create']);
 });
+
+$router->group(['prefix' => 'scrambler'], function () use ($router) {
+    $router->get('playground', [ScramblerController::class, 'playground']);
+    $router->post('create', [ScramblerController::class, 'create']);
+});
+
+$router->get('/score/get', [ScoreController::class, 'getScore']);
