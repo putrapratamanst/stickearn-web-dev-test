eab3906952c5d4b0e45e13ec77d568b4e2f9dbaa
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-04 17:34:14 +0700

    remove preloader

--- resources/views/scrambler/playground.blade.php
+++ resources/views/scrambler/playground.blade.php
@@ -1,19 +1,22 @@
 @extends('layouts.main')
 @section('content')
-
+<div class="preloader" style="display: none;">
+    <div class="loading">
+        <img src="{{ asset('assets/images/logo.gif') }}">
+    </div>
+</div>
 
 @include('layouts.navbar')
 <div class="gameArea">
     <h5 style="color:navy">
         SCORE: <span id="score"></span>
     </h5>
-    
     <div id="form-player" class="form-player">
         <h5 style="text-align:center;padding-bottom: 27px;">GUESS THE WORD:
             <span id="scramble-word"></span>
         </h5>
         <center>
-            {{ Form::open(array('url' => 'player/create', 'id' => 'form-playground', 'autocomplete'=>'off')) }}
+            {{ Form::open(array('id' => 'form-playground', 'autocomplete'=>'off')) }}
             <span id="counter">1</span>
             / 10
             {{ Form::text('guess_word', '', ['required']) }}
