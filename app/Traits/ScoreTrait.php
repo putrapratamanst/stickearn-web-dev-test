93c70e4c4718b73f3bdd7b06bb36eab6faffce20
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-04 12:33:29 +0700

    add function for delete score

--- app/Traits/ScoreTrait.php
+++ app/Traits/ScoreTrait.php
@@ -2,20 +2,44 @@
 
 namespace App\Traits;
 
+use App\Models\Score;
 use App\Repository\ScoreRepository;
 
 trait ScoreTrait
 {
-    private $repo;
-
-    public function __construct(ScoreRepository $repo)
+    public static function scoreByPlayer($playerId)
     {
-        $this->repo = $repo;
+        $repo = new ScoreRepository();
+        $repo->setPlayerId($playerId);
+        return $repo->scoreByPlayerId();
     }
 
-    public function scoreByPlayer($playerId)
+    public static function saveScore($playerId, $status)
     {
-        $this->repo->setPlayerId($playerId);
-        return $this->repo->scoreByPlayerId();
+        $score = $status == true ? 1 : -1;
+        $model             = new Score();
+        $model->player_id  = $playerId;
+        $model->score      = $score;
+
+        $model->save();
+    }
+
+    public static function deleteScore($playerId)
+    {
+        $model = self::scoreByPlayer($playerId);
+        if($model) {
+            $model->player_id  = $playerId;
+            $model->score      = 0;
+    
+            $model->save();
+        }
+    }
+
+    public static function updateScore($playerId, $status)
+    {
+        $detail = self::scoreByPlayer($playerId);
+        $score  = $status == true ? $detail['score'] + 1 : $detail['score'] + (-1);
+        $detail->score = $score;
+        $detail->save();
     }
 }
